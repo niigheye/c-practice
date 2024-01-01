984e5b47681c0187943894dab4a057ac6912f6c1
Sofya Grishkova <144588966+niigheye@users.noreply.github.com>
2024-01-01 17:52:27 +0300

    Create header.h

---
+++ 4/header.h
@@ -0,0 +1,9 @@
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+void bubble(int a[], int N,int p);
+void sele(int a[], int N, int s);
+void rbubble(int a[], int N);
+void rsele(int a[], int N);
+int iRandom(int k, int n);
